652fec15fce91817ac1186b4aef3c46b969cb4f6
Tobias Holm <201608355@post.au.dk>
2020-04-12 14:58:59 +0200

    Forms

--- Dab_aflevering_2/Pages/Index.cshtml.cs
+++ Dab_aflevering_2/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dab_aflevering_2.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -10,6 +11,7 @@ namespace Dab_aflevering_2.Pages
 {
     public class IndexModel : PageModel
     {
+      //  private readonly Repository<> repository;
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -19,6 +21,7 @@ namespace Dab_aflevering_2.Pages
 
         public void OnGet()
         {
+            
         }
     }
 }

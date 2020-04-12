579f7b28837217325ea668d8f29cac43e9a632a3
Tobias Holm <201608355@post.au.dk>
2020-04-12 14:24:17 +0200

    Added Repository.cs and IRepository.cs

--- Dab_aflevering_2/Repositories/Repository.cs
+++ Dab_aflevering_2/Repositories/Repository.cs
@@ -1,7 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dab_aflevering_2.Contracts;
+
 namespace Dab_aflevering_2.Repositories
 {
-    public class Repository
+    public class Repository<T> : IRepository<T> where T : class, IEntity
     {
-        
+        protected readonly DbContext context;
+
+        public Repository(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<List<T>> GetAll()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> Add(T entity)
+        {
+            context.Set<T>().Add(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> Delete(int id)
+        {
+            var entity = await context.Set<T>().FindAsync(id);
+            if (entity == null)
+            {
+                return entity;
+            }
+
+            context.Set<T>().Remove(entity);
+            await context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
     }
 }
